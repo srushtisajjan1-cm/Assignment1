23e1e246b3da2a46682b32c50bd5ab327d84fa08
Srushti Sajjan <srushtisajjan1@gmail.com>
2026-02-24 21:59:29 +0530

    added new files

---
+++ src/StudentTest.java
@@ -0,0 +1,30 @@
+class Student {
+    String name;
+    int age;
+
+    // Default constructor
+    Student() {
+        name = "Unknown";
+        age = 0;
+    }
+
+    // Parameterized constructor
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student();              // default
+        Student s2 = new Student("Srushti", 20); // parameterized
+
+        s1.display();
+        s2.display();
+    }
+}
